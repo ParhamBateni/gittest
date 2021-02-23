bcc82eeafcdba1996d328a8fb78f71496b23a7e4
ParhamBateni <mp.bateni@gmail.com>
2021-02-23 21:25:41 +0330

    Main2 added and it will make a sound!

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main{
 	public static void main(String[] args){
 		System.out.println("This will be printed");
+		Main2 sound=new Main2();
+		sound.dog();
 	}
 }
---
+++ Main2.java
@@ -0,0 +1,9 @@
+public class Main2{
+	public static void cat(){
+		System.out.println("Mio");
+	}
+	public static void dog(){
+		System.out.println("Hup");
+	}	
+
+}
